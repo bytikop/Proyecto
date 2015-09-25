a9a351c885b10bf642f8aff91edc6fea8fda20a9
bytikop <bytikop@gmail.com>
2015-09-25 13:01:24 -0600

    js

--- js/eventos.js
+++ js/eventos.js
@@ -1,1 +1,12 @@
-$
+//la function principal de la pagina
+var empiezaApp = function()
+{
+	var ValidarUsuario = function()
+	{
+		
+	}
+	$("#btnEntrar").on("click","ValidarUsuario");
+
+}
+// inicio de la pagina
+$(document).on("ready","empiezaApp");
